c1f0e9bb5792a6a1b2e04fc453fbfd4b16f3647f
ekgns14 <ekgns14@naver.com>
2021-06-05 15:41:39 +0900

    second commit

--- TableViewDesign/TableViewDesign/ViewController.swift
+++ TableViewDesign/TableViewDesign/ViewController.swift
@@ -17,3 +17,22 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? Cell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+    
+}
+
+class Cell: UITableViewCell {
+    
+}
+
