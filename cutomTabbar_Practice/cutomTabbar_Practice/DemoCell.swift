0706217f68dc14d5ba2a766809ab826226663d75
ekgns14 <ekgns14@naver.com>
2021-06-14 00:32:53 +0900

    노스토리보드 콜렉션뷰

---
+++ cutomTabbar_Practice/cutomTabbar_Practice/DemoCell.swift
@@ -0,0 +1,12 @@
+//
+//  DemoCell.swift
+//  cutomTabbar_Practice
+//
+//  Created by 다훈김 on 2021/06/14.
+//
+
+import UIKit
+
+class DemoCell: UICollectionViewCell {
+    
+}
--- cutomTabbar_Practice/cutomTabbar_Practice/ViewController.swift
+++ cutomTabbar_Practice/cutomTabbar_Practice/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+let cellID = "Cell"
+
 class ViewController: UIViewController {
     
     // MARK: - properties
@@ -14,7 +16,7 @@ class ViewController: UIViewController {
     let collectionView: UICollectionView = {
         let flowlayout = UICollectionViewFlowLayout()
         let cv = UICollectionView(frame: .zero, collectionViewLayout: flowlayout)
-        
+        flowlayout.scrollDirection = .vertical
         return cv
     }()
    
@@ -26,12 +28,52 @@ class ViewController: UIViewController {
         
         view.addSubview(collectionView)
         
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         
         collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         
+        collectionView.backgroundColor = .yellow
+        collectionView.register(DemoCell.self, forCellWithReuseIdentifier: cellID)
     }
-
 
 }
 
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! DemoCell
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (view.frame.width - 30 - 10) / 4, height: (view.frame.width - 30) / 4)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    }
+}
