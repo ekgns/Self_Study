1ef48d5cdb90e77dc694568d49f2803e708bd6f2
ekgns14 <ekgns14@naver.com>
2021-06-18 19:44:14 +0900

    Youtube Clone

---
+++ YouTubeMusic_clone/YouTubeMusic_clone/Home/HomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  HomeVC.swift
+//  YouTubeMusic_clone
+//
+//  Created by 다훈김 on 2021/06/18.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ YouTubeMusic_clone/YouTubeMusic_clone/Storage/StorageVC.swift
@@ -0,0 +1,43 @@
+//
+//  StorageVC.swift
+//  YouTubeMusic_clone
+//
+//  Created by 다훈김 on 2021/06/18.
+//
+
+import UIKit
+
+class StorageVC: UIViewController {
+
+    let contentImgs = ["download", "Playlists", "Album", "Song","Artist","Subscription"]
+    let contentNames = ["오프라인 저장 콘텐츠", "재생목록", "앨범", "노래", "아티스트", "구독"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
+extension StorageVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contentNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StorageListCell", for: indexPath) as! StorageListCell
+        cell.contentBtn.setTitle(contentNames[indexPath.row], for: .normal)
+        return cell
+    }
+    
+    
+}
+
+extension StorageVC: UITableViewDelegate {
+    
+}
+
+class StorageListCell: UITableViewCell {
+    @IBOutlet weak var contentBtn: UIButton!
+    
+}
