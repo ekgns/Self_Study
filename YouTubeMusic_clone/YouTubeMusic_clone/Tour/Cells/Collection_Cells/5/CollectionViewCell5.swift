692bd5c6c31284f49b4500e3c1912899b5d096ab
ekgns14 <ekgns14@naver.com>
2021-06-26 23:10:46 +0900

    새 뮤비 작업중

---
+++ YouTubeMusic_clone/YouTubeMusic_clone/Tour/Cells/Collection_Cells/5/CollectionViewCell5.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell5.swift
+//  YouTubeMusic_clone
+//
+//  Created by 다훈김 on 2021/06/26.
+//
+
+import UIKit
+
+class CollectionViewCell5: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
